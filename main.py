f2745ba334b094d636f944213517771dab1eb0b9
Tarini Hirendra Divgi <td508@ilab1.cs.rutgers.edu>
2024-04-26 21:04:50 -0400

    added logger and model loaders

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 import datetime
 import json
 import transformers
+from transformers.utils import logging
 import torch
+import time
+import datasets
 
 
 API_TOKEN_COUNTER = 0
@@ -14,18 +17,18 @@ def create_save_folder(args, start_time):
     save_folder_name = f"results/{args.output_name}{base_model_name}-{args.mask_filling_model}/{start_time}-{args.pct_words_masked}-{args.n_perturbation_rounds}-{args.dataset}-{args.n_samples}"
     if not os.path.exists(save_folder_name):
         os.makedirs(save_folder_name)
-    print(f"Saving results to path: {os.path.abspath(save_folder_name)}")
+    logger.info(f"Saving results to path: {os.path.abspath(save_folder_name)}")
     return save_folder_name
 
 def setup_cache(args):
     os.environ["XDG_CACHE_HOME"] = args.cache_dir
     if not os.path.exists(args.cache_dir):
         os.makedirs(args.cache_dir)
-    print(f"Cache directory: {args.cache_dir}")
+    logger.info(f"Cache directory: {args.cache_dir}")
 
-def load_base_model(model_name, cache_dir):
+def get_base_model(model_name, cache_dir):
     # no openai model not chosen
-    print(f'Loading Base model: {model_name}...')
+    logger.info(f'Loading Base model: {model_name}...')
     model_kwargs = {}
     if 'gpt-j' in model_name or 'neox' in model_name:
         model_kwargs.update(dict(torch_dtype=torch.float16))
@@ -37,10 +40,18 @@ def load_base_model(model_name, cache_dir):
 
     return base_model, base_tokenizer
 
+def load_base_model():
+    logger.info("Moving Base Model to GPU...", end='', flush=True)
+    start = time.time()
+    base_model.to(DEVICE)
+    logger.info(f'Done ({time.time() - start:.2f}s)')
 
 
 
 if __name__ == '__main__':
+    logging.set_verbosity_info()
+    logger = logging.get_logger("transformers")
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default="xsum")
     parser.add_argument('--pct-words-masked', type=float, default=0.3) # pct masked is actually pct_words_masked * (span_length / (span_length + 2 * buffer_size))
@@ -53,13 +64,13 @@ if __name__ == '__main__':
     parser.add_argument('--chunk-size', type=int, default=20)
     parser.add_argument('--cache-dir', type=str, default=".cache")
     parser.add_argument('--output-name', type=str, default="n_perturb")
-    parser.add_argument('--baselines_only', action='store_true')
-    parser.add_argument('--random_fills', action='store_true')
+    parser.add_argument('--baseline-only', action='store_true')
+    parser.add_argument('--random-fills', action='store_true')
     args = parser.parse_args()
 
-    print(args.dataset)
-    print(args.mask_filling_model)
-    print(args.n_perturbation_list)
+    logger.info(args.dataset)
+    logger.info(args.mask_filling_model)
+    logger.info(args.n_perturbation_list)
 
     # torch.cuda.empty_cache()
 
@@ -75,14 +86,24 @@ if __name__ == '__main__':
         json.dump(args.__dict__, f, indent=2)
 
     setup_cache(args)
-
     perturbation_list = [int(x) for x in args.n_perturbation_list.split(",")]
-
-    base_model, base_tokenizer = load_base_model(args.base_model, args.cache_dir)
-    
+    base_model, base_tokenizer = get_base_model(args.base_model, args.cache_dir)
     
     GPT2_TOKENIZER = transformers.GPT2Tokenizer.from_pretrained('gpt2', cache_dir=args.cache_dir)
-        
 
+    # considering only baselines and random fills
+    #considering int8
+    kwargs = dict(load_in_8bit=True, device_map="auto", torch_dtype=torch.bfloat16)
+    logger.info(f'Loading mask filling model {args.mask_filling_model}')
+    
+    # use sequence-to-sequence LM since we want to map the input text to a masked output text
+    # mask_model = transformers.AutoModelForSeq2SeqLM.from_pretrained(args.mask_filling_model, **kwargs, cache_dir=args.cache_dir)
+    
+    pre_tokenizer = transformers.AutoTokenizer.from_pretrained("t5-small", model_max_length=256, cache_dir=args.cache_dir)
+    mask_tokenizer = transformers.AutoTokenizer.from_pretrained(args.mask_filling_model, model_max_length=256, cache_dir=args.cache_dir)
+
+    load_base_model(base_model)
+
+    
 
     
